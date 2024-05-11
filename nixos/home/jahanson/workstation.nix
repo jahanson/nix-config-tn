e43a4f4e736f8e9e9ae8671688fd1919996822ef
jahanson <joe@veri.dev>
2024-05-11 08:23:27 -0500

    Revert packages.

--- nixos/home/jahanson/workstation.nix
+++ nixos/home/jahanson/workstation.nix
@@ -32,9 +32,52 @@ with config;
         vlc
 
         # cli
+        _1password
+        bat
+        dbus
+        direnv
+        git
+        nix-index
+        python3
+        fzf
+        ripgrep
         brightnessctl
+        fastfetch
 
-        # dev utils
+        # terminal file managers
+        nnn
+        ranger
+        yazi
+
+        # networking tools
+        iperf3
+        dnsutils  # `dig` + `nslookup`
+        ldns # replacement of `dig`, it provide the command `drill`
+        aria2 # A lightweight multi-protocol & multi-source command-line download utility
+        socat # replacement of openbsd-netcat
+        nmap # A utility for network discovery and security auditing
+        ipcalc  # it is a calculator for the IPv4/v6 addresses
+
+        # system tools
+        sysstat
+        lm_sensors # for `sensors` command
+        ethtool
+        pciutils # lspci
+        usbutils # lsusb
+
+        # system call monitoring
+        strace # system call monitoring
+        ltrace # library call monitoring
+        lsof # list open files
+
+        btop # replacement of htop/nmon
+        iotop # io monitoring
+        iftop # network monitoring
+
+        # utils
+        direnv # shell environment management
+        pre-commit # Pre-commit tasks for git
+        minio-client # S3 management
         shellcheck
         envsubst
       ];
